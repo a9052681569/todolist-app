33071bc72bfd516fd10280abc0b9e33ae67d8868
aasereb6 <aasereb6@mts.ru>
2022-10-27 19:22:20 +0300

    feat: рендер элементов списка из js

--- script.js
+++ script.js
@@ -3,4 +3,30 @@ const items = [
 	"Полить цветы",
 	"Пройти туториал по Реакту",
 	"Сделать фронт для своего проекта",
+	"Сделать фронт для своего проекта",
+	"Сделать фронт для своего проекта",
+	"Сделать фронт для своего проекта",
 ];
+
+const container = document.querySelector('.list');
+
+const getTodoItemHtml = (text) => {
+	return `
+	<li class="list__item">
+		<span class="item__text">${text}</span>
+		<img
+			class="list__item-btn list__item-btn_duplicate"
+			src="images/Duplicate.png"
+			alt="Копировать"
+		/>
+		<img class="list__item-btn list__item-btn_delete" src="images/Delete.png" alt="Удалить" />
+	</li>
+`
+}
+
+items.forEach((item) => {
+	const elString = getTodoItemHtml(item);
+
+	container.insertAdjacentHTML('afterbegin', elString)
+})
+
